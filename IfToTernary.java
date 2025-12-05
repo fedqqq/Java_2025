6470bb38b352aae2d760d64ebac3730fb80adfac
Fedor Fil <fedq1105@mail.ru>
2025-12-05 19:50:21 +0300

    Практика по теме оператор условного перехода

---
+++ IfToTernary.java
@@ -0,0 +1,7 @@
+public class IfToTernary {
+    public static void main(String[] args) {
+        int a = 2;
+        int i2 = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+        System.out.println("Результат с ? : " + i2);
+    }
+}
